e61eaea93f9c2c2e076fda7c31ae1d77e1939afb
devanshparmar <devanshparmar@EZDI-LAP0131>
2022-03-06 16:06:03 +0530

    print new message

---
+++ src/main/java/training/fetchData/App.java
@@ -0,0 +1,14 @@
+package training.fetchData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("south east asia");
+    }
+}
