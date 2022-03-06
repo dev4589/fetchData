9acc99d575e95894de3e303e08564056ca4021c6
devanshparmar <devanshparmar@EZDI-LAP0131>
2022-03-06 16:06:03 +0530

    fresh

---
+++ src/main/java/training/fetchData/App.java
@@ -0,0 +1,13 @@
+package training.fetchData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
