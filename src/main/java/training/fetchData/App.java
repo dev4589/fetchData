06281f2df5b7afdc34602ecfdf4af8e656251197
devanshparmar <devanshparmar@EZDI-LAP0131>
2022-03-07 12:57:57 +0530

    print data from table

--- src/main/java/training/fetchData/App.java
+++ src/main/java/training/fetchData/App.java
@@ -1,5 +1,11 @@
 package training.fetchData;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
@@ -8,12 +14,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("hello");
-        int i=10;
-        System.out.println(i);
-        System.out.println("hello from up top");
-        System.out.println("new config");
-        //dev
+        String url="jdbc:mysql://localhost:3306/universityData";
+        String user="root";
+        String pwd="rootdevansh";
+        
+        try {
+        	Class.forName("com.mysql.jdbc.Driver");
+        	Connection connection = DriverManager.getConnection(url,user,pwd);
+        	System.out.println("Connection to database " + url + " is successful");
+        	String query="select * from studentData";
+        	Statement statement=connection.createStatement();
+        	ResultSet results=statement.executeQuery(query);
+        	while(results.next()) {
+        		int id=results.getInt(1);
+        		String name=results.getString("name");
+        		System.out.println("ID: "+id+" Name: "+name);
+        	}
+        }
+        catch(ClassNotFoundException e) {
+        	e.printStackTrace();
+        }
+        catch(SQLException throwables) {
+        	throwables.printStackTrace();
+        }
     }
 }
